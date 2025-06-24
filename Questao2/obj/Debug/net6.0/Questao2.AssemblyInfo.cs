8d3d2c0023c004bb53f97f19e075b5820af0e77a
alesson.garcia <alesson.garcia@connect.com.vc>
2025-06-24 16:07:41 -0400

    Ajustes finais!

--- Questao2/obj/Debug/net6.0/Questao2.AssemblyInfo.cs
+++ Questao2/obj/Debug/net6.0/Questao2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Questao2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a0f10373e72eac5e8ddc4b3da30845100d274f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Questao2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Questao2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
